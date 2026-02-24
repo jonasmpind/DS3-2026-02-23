0117bee57bd65b8036a6be73dbe6306e4994e489
jonasmpind <jonasmpind@gmail.com>
2026-02-24 07:20:53 +0100

    Update style-dictionary.config.js

--- token-docs/style-dictionary.config.js
+++ token-docs/style-dictionary.config.js
@@ -1,13 +1,18 @@
+const StyleDictionary = require('style-dictionary');
+
 module.exports = {
   source: [
-    '../tokens/base/**/*.json',
-    '../tokens/web/**/*.json'
+    'tokens/base/core.json',
+    'tokens/base/semantic.json',
+    'tokens/base/component.json',
+    'tokens/web/core.json',
+    'tokens/web/semantic.json',
+    'tokens/web/component.json'
   ],
-
   platforms: {
     docs: {
-      transformGroup: 'js',
-      buildPath: 'build/',
+      transforms: [], // important: do not transform
+      buildPath: 'token-docs/build/',
       files: [
         {
           destination: 'tokens.json',
